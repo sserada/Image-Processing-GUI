6563f8ab39562305c211f7bd479517e0cb450690
fightingsou <hirawatasou@gmail.com>
2023-08-07 13:54:43 +0900

    :sparkles: Improve image processing and enhance backend
    This commit includes several major improvements related to image processing and backend functionality.
    
    First, the Dockerfile is modified to update packages and install OpenCV for image processing. In the main API, the received image is now converted to grayscale using OpenCV, and the requirements file includes the OpenCV Python package.
    
    Second, a new file 'image.jpg' is added, and changes are made to 'main.py' in the API and backend folders to handle image uploads and processing more efficiently. The backend is refactored to handle images more efficiently, with new functions to save and send processed images.
    
    Third, the 'default.conf' file is updated to set the client's maximum body size, allowing for larger image uploads.
    
    Overall, these changes enhance the application's performance and user experience by allowing users to upload and process larger images more efficiently.

--- api/main.py
+++ api/main.py
@@ -1,4 +1,5 @@
 import os
+import cv2
 import json
 import base64
 import uvicorn
@@ -10,9 +11,19 @@ app = FastAPI()
 @app.post('/api/upload/')
 async def image_processing(file: UploadFile = File(...)):
     image_content = await file.read()
+    print('Received image')
     with open('image.jpg', 'wb') as f:
         f.write(image_content)
-    encoded_string = base64.b64encode(image_content).decode()
+
+    # Write the code to process the image here.
+    # For example, read the image using OpenCV and convert it to grayscale.
+    img = cv2.imread('image.jpg')
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    cv2.imwrite('image.jpg', gray)
+    with open('image.jpg', 'rb') as f:
+        encoded_string = base64.b64encode(f.read()).decode()
+        print(encoded_string)
+
     response = {'processed_image': encoded_string}
     return response
 
--- backend/main.py
+++ backend/main.py
@@ -12,6 +12,35 @@ app = FastAPI()
 connected_clients = []
 
 images = {}
+image_num = {}
+
+def handle_image(client_id, data, images):
+    if (images.get(str(data['name']).split('.')[0]) == None):
+        images[str(data['name']).split('.')[0]] = ''
+    images[str(data['name']).split('.')[0]] += str(data['data'])
+    if data['index'] == data['total']:
+        image_num[str(client_id)] = image_num.get(str(client_id), 0) + 1
+        print(image_num[str(client_id)])
+        format, image = images[str(data['name']).split('.')[0]].split(',')
+        image = base64.b64decode(image)
+        save_image(client_id, data['name'], image)
+        send_api_request(client_id, data['name'], image)
+
+def save_image(client_id, name, image):
+    if not os.path.exists('images/' + str(client_id)):
+        os.makedirs('images/' + str(client_id))
+    with open('images/' + str(client_id) + '/' + str(name), 'wb') as f:
+        f.write(image)
+
+def send_api_request(client_id, name, image):
+    files = {'file': image}
+    response = requests.post('http://api:8001/api/upload', files=files)
+    response = response.json()
+    processed_image = response['processed_image']
+    print(processed_image)
+    processed_image = base64.b64decode(processed_image)
+    with open('images/' + str(client_id) + '/processed_' + str(name), 'wb') as f:
+        f.write(processed_image)
 
 @app.websocket('/backend/websocket/{client_id}')
 async def upload_endpoint(websocket: WebSocket, client_id: str, background_task: BackgroundTasks):
@@ -20,27 +49,10 @@ async def upload_endpoint(websocket: WebSocket, client_id: str, background_task:
     try:
         while True:
             data = await websocket.receive_json()
-            if (images.get(str(data['name']).split('.')[0]) == None):
-                images[str(data['name']).split('.')[0]] = ''
-            images[str(data['name']).split('.')[0]] += str(data['data'])
-            if data['index'] == data['total']:
-                format, image = images[str(data['name']).split('.')[0]].split(',')
-                print(image)
-                image = base64.b64decode(image)
-                if not os.path.exists('images/' + str(client_id)):
-                    os.makedirs('images/' + str(client_id))
-                with open('images/' + str(client_id) + '/' + str(data['name']), 'wb') as f:
-                    f.write(image)
-                images[str(data['name']).split('.')[0]] = ''
-                files = {'file': image}
-                response = requests.post('http://api:8001/api/upload', files=files)
-                response = response.json()
-                processed_image = response['processed_image']
-                processed_image = base64.b64decode(processed_image)
-                with open('images/' + str(client_id) + '/processed_' + str(data['name']), 'wb') as f:
-                    f.write(processed_image)
+            handle_image(client_id, data, images)
     except:
         connected_clients.remove(websocket)
 
 if __name__ == '__main__':
     uvicorn.run(app, host='0.0.0.0', port=8000)
+
