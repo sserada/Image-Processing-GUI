33b7f0016190518a35b131d5bac88c868f124afc
fightingsou <hirawatasou@gmail.com>
2023-08-28 02:08:18 +0900

    :sparkles: Implement dynamic image path and refactor Docker compose
    With these changes, we have made two main modifications. Firstly, we have integrated a dynamic image path based on the environment's port. This ensures that each instance will have its own unique storage location, preventing potential overwrites and conflicts.
    
    Secondly, there is a significant refactor of the 'compose.yml'. The 'nginx' configuration is removed, and instead, a new service named 'api2' is added. This change aims to simplify the setup and use a dedicated API instance with its port.
    
    The rationale behind these changes is to enhance scalability, maintainability, and reduce complexities associated with the previous 'nginx' setup. Additionally, the dynamic image path will allow better flexibility when deploying multiple instances of the application.

--- api/main.py
+++ api/main.py
@@ -7,24 +7,30 @@ from fastapi import FastAPI, File, UploadFile, Form, Request
 from fastapi.responses import HTMLResponse
 
 app = FastAPI()
+image_path = 'images/' + os.environ['PORT'] + '.jpg'
 
-@app.post('/api/upload/')
+@app.on_event("startup")
+async def startup_event():
+    # You can load neural networks, etc. here.
+    if not os.path.exists('images'):
+        os.makedirs('images')
+
+@app.post('/api/upload')
 async def image_processing(file: UploadFile = File(...)):
     image_content = await file.read()
     print('Received image')
-    with open('image.jpg', 'wb') as f:
+    with open(image_path, 'wb') as f:
         f.write(image_content)
 
     # Write the code to process the image here.
     # For example, read the image using OpenCV and convert it to grayscale.
-    img = cv2.imread('image.jpg')
+    img = cv2.imread(image_path)
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    cv2.imwrite('image.jpg', gray)
-    with open('image.jpg', 'rb') as f:
+    cv2.imwrite(image_path, gray)
+    with open(image_path, "rb") as f:
         encoded_string = base64.b64encode(f.read()).decode()
-        print(encoded_string)
-
-    response = {'processed_image': encoded_string}
+    os.remove(image_path)
+    response = {'processed': encoded_string}
     return response
 
 if __name__ == '__main__':
